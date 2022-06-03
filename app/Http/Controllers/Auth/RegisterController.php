23f9681bc42b555662fa1ef910444782b7d3e530
raminhaghighitalab <76012089+raminhaghighitalab@users.noreply.github.com>
2022-06-03 12:58:11 -0400

    Update RegisterController.php

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -39,7 +39,7 @@ class RegisterController extends Controller
     public function __construct()
     {
 //to dystroy registeration
-        return redirect->to('/')->send();
+        //return redirect->to('/')->send();
         $this->middleware('auth');
 
     }
