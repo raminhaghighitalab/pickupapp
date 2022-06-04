bda124559c771fc6dafb81566aa90eac8d398b2e
raminhaghighitalab <76012089+raminhaghighitalab@users.noreply.github.com>
2022-06-03 22:45:44 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -753,7 +753,7 @@
     </section><!-- End Contact Section -->
 
   </main><!-- End #main -->
-
+<section id="footer">
   <!-- ======= Footer ======= -->
   <footer id="footer">
     <div class="footer-top">
@@ -827,7 +827,7 @@
       </div>
     </div>
   </footer><!-- End Footer -->
-
+  </section>
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
